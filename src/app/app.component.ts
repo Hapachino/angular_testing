511b60befd9d4c47a9215c0445685aceabff7e60
Hapachino <erickwbrownfield@gmail.com>
2019-03-15 16:25:07 -0700

    restarted html

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -7,37 +7,5 @@ import { NgForm } from '@angular/forms';
   styleUrls: ['./app.component.css']
 })
 export class AppComponent {
-  @ViewChild('f') signUpForm: NgForm;
-  numbers = [];
-  default = '';
-  prices = [100, 200, 300];
 
-  user = {
-    email: '',
-    price: '',
-  }
-
-  defaultEmail() {
-    this.signUpForm.form.patchValue({
-      userData: {
-        email: 'hello',
-      },
-      password: 'hey',
-    });
-    console.log(this.user);
-  }
-
-  onNumberCreated(numberData: { number: number }) {
-    this.numbers.push({ number: numberData.number });
-  }
-
-  onSubmit(form: NgForm) {
-    const { userData, price } = this.signUpForm.value;
-    const { user } = this;
-
-    user.email = userData.email;
-    user.price = price;
-
-    this.signUpForm.reset();
-  }
 }
